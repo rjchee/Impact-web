9eace56d22fae8477a2738c109f7440f04809421
Raymond Chee <rjchee@gmail.com>
2018-01-27 17:13:34 -0600

    Add db migrate dependencies

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 import settings
 
@@ -8,6 +10,7 @@ app = Flask(__name__)
 app.config.from_object(settings)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=settings.PORT, debug=settings.DEBUG)
