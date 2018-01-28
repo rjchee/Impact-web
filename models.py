73dfd34332b14dfc8cd181fbd52b038975070ba8
Raymond Chee <rjchee@gmail.com>
2018-01-28 04:01:34 -0600

    Record donation history

--- models.py
+++ models.py
@@ -42,7 +42,7 @@ class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Numeric(10, 2), nullable=False)
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
-    categories = db.relationship('Category', secondary=account_table, backref='entries')
+    categories = db.relationship('Category', secondary=account_table)
 
     def __init__(self, val, categories):
         self.value = val
@@ -55,3 +55,25 @@ class Category(db.Model):
 
     def __init__(self, n):
         self.name = n
+
+
+donation_table = db.Table('donation_helper',
+    db.Column('donation_id', db.Integer, db.ForeignKey('donation.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+)
+
+
+class Donation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Numeric(10, 2), nullable=False)
+    categories = db.relationship('Category', secondary=donation_table)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    sender = db.relationship('User', backref='sent_donations', foreign_keys=[sender_id])
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    receiver = db.relationship('User', backref='received_donations', foreign_keys=[receiver_id])
+
+    def __init__(self, sender, receiver, value, categories):
+        self.sender = sender
+        self.receiver = receiver
+        self.value = value
+        self.categories = Category.query.filter(Category.name.in_(categories)).all()
--- views.py
+++ views.py
@@ -1,9 +1,10 @@
+from collections import defaultdict
 from decimal import Decimal
 
 from flask import jsonify, render_template, request
 
 from app import app, db
-from models import User, Account, Entry, Category
+from models import *
 
 def error(message):
     return jsonify({'error': message, 'success': False}), 400
@@ -20,8 +21,8 @@ def create_user():
     except:
         return error('the username already exists')
 
-@app.route('/api/user/add', methods=['POST'])
-def add_money():
+@app.route('/api/user/donate', methods=['POST'])
+def donate_money():
     sender = request.form.get('sender')
     receiver = request.form.get('receiver')
     value = request.form.get('value')
@@ -43,23 +44,46 @@ def add_money():
     rcver = User.query.filter_by(username=receiver).first()
     if rcver is None:
         return error('user {} is not found'.format(receiver))
+    snder = User.query.filter_by(username=sender).first()
+    if snder is None:
+        return error('user {} is not found'.format(sender))
     rcver.account.add_entry(value, categories)
+    donation = Donation(snder, rcver, value, categories)
+    db.session.add(donation)
+    rcver.received_donations.append(donation)
+    snder.sent_donations.append(donation)
     db.session.commit()
     return jsonify({'success': True})
 
+
+def consolidate_donations(donations):
+    aggregate = defaultdict(float)
+    for donation in donations:
+        aggregate[tuple(sorted(c.name for c in donation.categories))] += float(donation.value)
+    result = []
+    for categories, value in aggregate.items():
+        result.append({
+            'categories': list(categories),
+            'value': value
+        })
+    return result
 
 @app.route('/api/user/<username>', methods=['GET'])
 def get_account(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         return error('user {} was not found'.format(username))
-    entries = []
+    balance = []
     for e in user.account.entries:
-        entries.append({
-            'categories': [c.name for c in e.categories],
+        balance.append({
+            'categories': sorted([c.name for c in e.categories]),
             'value': float(e.value)
         })
-    result = {'success': True, 'entries': entries}
+    sent_donations = consolidate_donations(user.sent_donations)
+    print(user.sent_donations, sent_donations)
+    received_donations = consolidate_donations(user.received_donations)
+    print(user.received_donations, received_donations)
+    result = {'success': True, 'balance': balance, 'sent_donations_breakdown': sent_donations, 'received_donations_breakdown': received_donations}
     return jsonify(result)
 
 
